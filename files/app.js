abe1d9e77dbe2db0de42e3cd27b6951767a18674
Hamza Sufian <hsufiian@yahoo.com>
2024-02-03 18:43:38 +0000

    Add database connection and update server port

--- files/app.js
+++ files/app.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const { connectToDb, getDb } = require('./db');
 
 // Create an Express application
 const app = express();
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-});
+// db connection
+let db;
+
+// Connect to the database
+connectToDb((err) => {
+    if (!err) {
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
+        db = getDb();
+    }
+})
 
 // Create a GET route
 app.get('/books', (req, res) => {
