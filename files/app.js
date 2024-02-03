964f6e384053f27221caf1719c4f8edde1aa3fe4
Hamza Sufian <hsufiian@yahoo.com>
2024-02-03 18:14:23 +0000

    Add Express server and create a GET route

--- files/app.js
+++ files/app.js
@@ -1,1 +1,13 @@
-console.log(" hey ninjas")
+const express = require('express');
+
+// Create an Express application
+const app = express();
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
+
+// Create a GET route
+app.get('/books', (req, res) => {
+    res.json('Welcome to the home page!');
+})
--- files/main.js
+++
@@ -1,7 +0,0 @@
-let year = prompt('In which year was ECMAScript-2015 specification published?', '');
-
-if (year == 2015) {
-  console.log( 'You are right!' );
-} else {
-  console.log( 'You are wrong.' ); // any value except 2015
-}
