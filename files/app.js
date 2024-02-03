324acabaf3fb7cf8064463f2f4b8ffbddabe1219
Hamza Sufian <hsufiian@yahoo.com>
2024-02-03 20:32:15 +0000

    get books

--- files/app.js
+++ files/app.js
@@ -10,7 +10,7 @@ let db;
 // Connect to the database
 connectToDb((err) => {
     if (!err) {
-        app.listen(3000, () => {
+        app.listen(5000, () => {
             console.log('Server is running on port 3000');
         });
         db = getDb();
@@ -19,5 +19,15 @@ connectToDb((err) => {
 
 // Create a GET route
 app.get('/books', (req, res) => {
-    res.json('Welcome to the home page!');
-})
+    let books = [];
+    db.collection('books')
+        .find() // find all books
+        .sort({ author: 1 }) // sort by author name
+        .forEach(book => books.push(book)) // push each book to the books array
+        .then(() => {
+            res.status(200).json(books);
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'Could not fetch the documents' });
+        })
+});
