f70f3db7363862a4873027ba1aae17229e7e20f4
Hamza Sufian <hsufiian@yahoo.com>
2024-02-09 15:12:41 +0000

    Refactor app.js to improve readability and add pagination to GET /books route

--- files/app.js
+++ files/app.js
@@ -3,8 +3,8 @@ const { connectToDb, getDb } = require('./db');
 const { ObjectId } = require('mongodb');
 
 // Create an Express application
-const app = express();
-app.use(express.json());
+const app = express(); // create an express app
+app.use(express.json()); // use the json middleware to parse the request body
 
 // db connection
 let db;
@@ -12,36 +12,42 @@ let db;
 // Connect to the database
 connectToDb((err) => {
     if (!err) {
-        app.listen(5000, () => {
+        app.listen(5000, () => { // listen on port 5000
             console.log('Server is running on port 5000');
         });
-        db = getDb();
+        db = getDb(); // get the database object
     }
 })
 // Create a GET route
 app.get('/books', (req, res) => {
-    const page = req.query.page || 0
-    const booksPerPage = 3
+    const page = req.query.page || 0 // get the page number from the query string or use 0 if it's not provided
+    const booksPerPage = 3 // number of books to show per page
+
+    // find all books in the database
     let books = [];
     db.collection('books')
         .find() // find all books
         .sort({ author: 1 }) // sort by author name
+        .skip(page * booksPerPage) // skip the first n books
+        .limit(booksPerPage) // limit the number of books to n
         .forEach(book => books.push(book)) // push each book to the books array
-        .then(() => {
-            res.status(200).json(books);
+        .then(() => { // when the forEach is done
+            res.status(200).json(books); // send the books array as a JSON response
         })
-        .catch(() => {
+        .catch(() => { // if there is an error
+            // send a 500 Internal Server Error response
             res.status(500).json({ error: 'Could not fetch the documents' });
         })
 });
 
-app.get('/books/:id', (req, res) => {
-    if (ObjectId.isValid(req.params.id)) {
-        db.collection('books')
-        .findOne({ _id: new ObjectId(req.params.id) })
-        .then(doc => {
-            if (doc) {
-                res.status(200).json(doc);
+app.get('/books/:id', (req, res) => { // get a book by id
+    // check if the id is valid
+    if (ObjectId.isValid(req.params.id)) { // if the id is valid
+        db.collection('books') // get the books collection
+        .findOne({ _id: new ObjectId(req.params.id) }) // find the book with the given id
+        .then(doc => {  // when the book is found
+            if (doc) { // if the book exists
+                res.status(200).json(doc); //
             }
         })
         .catch(() => {
