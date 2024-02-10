b5bce2761788dbd999f5e817e8170629e0445711
Hamza Sufian <hsufiian@yahoo.com>
2024-02-10 18:53:55 +0000

    Update MongoDB connection URI

--- files/db.js
+++ files/db.js
@@ -1,11 +1,11 @@
 const { MongoClient } = require('mongodb');
 let dbConnection;
-let url = 'mongodb+srv://hamzasufian2014:1MZQ7KE5Z7djgmGb@cluster0.ovpb1oc.mongodb.net/?retryWrites=true&w=majority'
+let uri = 'mongodb+srv://hamzasufian2014:1MZQ7KE5Z7djgmGb@cluster0.ovpb1oc.mongodb.net/?retryWrites=true&w=majority'
 // database init connection
 
 module.exports = {
     connectToDb: (cb) => {
-        MongoClient.connect(url)
+        MongoClient.connect(uri)
         .then((client) => {
             dbConnection = client.db()
             return cb()
