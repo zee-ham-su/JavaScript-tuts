44f75a2b37a5e962875f3036c6e45c4929ba5489
Hamza Sufian <hsufiian@yahoo.com>
2024-02-27 20:48:04 +0000

    Update blog.txt and file.js

--- files/node_crash_course/file.js
+++ files/node_crash_course/file.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+
+// reading files
 fs.readFile('./docs/blog.txt', (err, data) => {
     if (err) {
         console.log(err);
@@ -9,12 +11,28 @@ fs.readFile('./docs/blog.txt', (err, data) => {
 
 console.log('last line');
 
-// reading files
 
 // writing files
+fs.writeFile('./docs/blog.txt', 'hello, world', () => {
+    console.log('file was written');
+});
 
+fs.writeFile('./docs/blog2.txt', 'hello, again', () => {
+    console.log('file was written');
+});
 
 // directories
-
+fs.mkdir('./assets', (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log('folder created');
+});
 
 //deleting files
+fs.unlink('./docs/blog2.txt', (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log('file deleted');
+});
