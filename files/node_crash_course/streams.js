f1d5e4c22734b4c793ac5474cb2ed434571158d4
Hamza Sufian <hsufiian@yahoo.com>
2024-03-01 19:50:09 +0000

    streams and buffer

---
+++ files/node_crash_course/streams.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+const readStream = fs.createReadStream('./docs/blog3.txt');
+readStream.on('data', (chunk) => {
+    console.log('--- NEW CHUNK ---');
+    console.log(chunk);
+});
