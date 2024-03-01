40f6a110a14859135318d3939edce1ff4dc8257a
Hamza Sufian <hsufiian@yahoo.com>
2024-03-01 19:52:14 +0000

    Update readStream to include encoding option

--- files/node_crash_course/streams.js
+++ files/node_crash_course/streams.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const readStream = fs.createReadStream('./docs/blog3.txt');
+const readStream = fs.createReadStream('./docs/blog3.txt', 'utf8');
 
 readStream.on('data', (chunk) => {
     console.log('--- NEW CHUNK ---');
