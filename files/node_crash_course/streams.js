d141f2a4d4fc95f9fed315e652289902d70a694c
Hamza Sufian <hsufiian@yahoo.com>
2024-03-01 19:54:08 +0000

    Add write stream to append new chunk to file

--- files/node_crash_course/streams.js
+++ files/node_crash_course/streams.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 
 const readStream = fs.createReadStream('./docs/blog3.txt', 'utf8');
+const writeStream = fs.createWriteStream('./docs/blog4.txt');
+
 
 readStream.on('data', (chunk) => {
     console.log('--- NEW CHUNK ---');
     console.log(chunk.toString());
+    writeStream.write('\nNEW CHUNK\n');
+    writeStream.write(chunk);
 });
