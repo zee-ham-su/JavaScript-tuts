5452c23b12e1cdefcb5c21938be405efe812518d
Hamza Sufian <hsufiian@yahoo.com>
2024-03-02 10:58:16 +0000

    Refactor stream handling in streams.js

--- files/node_crash_course/streams.js
+++ files/node_crash_course/streams.js
@@ -4,12 +4,12 @@ const readStream = fs.createReadStream('./docs/blog3.txt', 'utf8');
 const writeStream = fs.createWriteStream('./docs/blog4.txt');
 
 
- // readStream.on('data', (chunk) => {
-   // console.log('--- NEW CHUNK ---');
-   // console.log(chunk.toString());
-   // writeStream.write('\nNEW CHUNK\n');
-   // writeStream.write(chunk);
-// });
+  readStream.on('data', (chunk) => {
+    console.log('--- NEW CHUNK ---');
+    console.log(chunk.toString());
+    writeStream.write('\nNEW CHUNK\n');
+    writeStream.write(chunk);
+});
 
 // piping
 readStream.pipe(writeStream);
