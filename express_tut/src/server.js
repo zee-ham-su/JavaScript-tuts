d449437cc5e7cafd01668cb8ba51369b11981b2d
Hamza Sufian <hsufiian@yahoo.com>
2024-04-19 11:54:52 +0000

    Update response status code and message in server.js

--- express_tut/src/server.js
+++ express_tut/src/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.status(201).send({msg: 'Hello World!'});
 });
 
 app.listen(PORT, () => {
