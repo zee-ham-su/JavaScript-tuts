8044b84190363d88a84d40d57cac09627590c722
Hamza Sufian <hsufiian@yahoo.com>
2024-04-19 12:04:21 +0000

    Add API routes for users and products in server.js

--- express_tut/src/server.js
+++ express_tut/src/server.js
@@ -7,6 +7,24 @@ app.get('/', (req, res) => {
   res.status(201).send({msg: 'Hello World!'});
 });
 
+app.get('/api/users', (req, res) => {
+  res.send([
+    { id: 1, name: 'John Doe'},
+    { id: 2, name: 'Jane Doe'},
+    { id: 3, name: 'John Smith'},
+    { id: 4, name: 'Jane Smith'},
+  ]);
+});
+
+app.get("/api/products", (req, res) => {
+  res.send([
+    { id: 1, name: 'phone', price: 1000.00 },
+    { id: 2, name: 'laptop', price: 2000.99 },
+    { id: 3, name: 'tablet', price: 1500.50 },
+    { id: 4, name: 'desktop', price: 2500.75 },
+  ]);
+});
+
 app.listen(PORT, () => {
   console.log('Server is running on port', PORT)
 });
