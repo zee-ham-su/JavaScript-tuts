e0384a30734a6e9a5e81a3d702ec11fadfcd5c3a
Hamza Sufian <hsufiian@yahoo.com>
2024-04-19 12:24:44 +0000

    Refactor API routes and add error handling in server.js

--- express_tut/src/server.js
+++ express_tut/src/server.js
@@ -3,17 +3,20 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.status(201).send({msg: 'Hello World!'});
-});
-
-app.get('/api/users', (req, res) => {
-  res.send([
+const dummyData = [
     { id: 1, name: 'John Doe'},
     { id: 2, name: 'Jane Doe'},
     { id: 3, name: 'John Smith'},
     { id: 4, name: 'Jane Smith'},
-  ]);
+  ];
+
+
+  app.get('/', (req, res) => {
+  res.status(201).send({msg: 'Hello World!'});
+});
+
+app.get('/api/users', (req, res) => {
+  res.send(dummyData);
 });
 
 app.get("/api/products", (req, res) => {
@@ -24,6 +27,22 @@ app.get("/api/products", (req, res) => {
     { id: 4, name: 'desktop', price: 2500.75 },
   ]);
 });
+
+
+app.get('/api/users/:id', (req, res) => {
+  console.log(req.params);
+  const parsedId = parseInt(req.params.id);
+  console.log(parsedId);
+  if (isNaN(parsedId)) {
+    res.status(400).send({msg: 'Invalid ID supplied'});
+  };
+  const findUser = dummyData.find(user => user.id === parsedId);
+  if (!findUser) {
+    res.status(404).send({msg: 'User not found'});
+    return res.send(findUser);
+  };
+});
+
 
 app.listen(PORT, () => {
   console.log('Server is running on port', PORT)
