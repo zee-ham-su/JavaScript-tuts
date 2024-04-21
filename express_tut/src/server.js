371eeb96f37b60f78bc0e8a57a8a0cd757a8aa9b
Hamza Sufian <hsufiian@yahoo.com>
2024-04-21 12:41:39 +0000

    Refactor user update logic and add error handling in server.js

--- express_tut/src/server.js
+++ express_tut/src/server.js
@@ -78,10 +78,11 @@ app.put('/api/users/:id', (req, res) => {
     return res.status(400).send({msg: 'Invalid ID supplied'});
   };
   const findUserIndex = dummyData.findIndex((user) => user.id === parsedId);
-  if (findUserIndex === -1) {
+  if (findUserIndex === -1)
     return res.status(404).send({msg: 'User not found'});
 
   dummyData[findUserIndex] = { id: parsedId, ...body };
+  return res.sendStatus(204);
 });
 
 app.listen(PORT, () => {
