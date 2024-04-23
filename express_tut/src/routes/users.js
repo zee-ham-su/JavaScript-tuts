44b71fcb0f137bb630da03a4419e418ef0b2c7fb
Hamza Sufian <hsufiian@yahoo.com>
2024-04-23 20:26:18 +0000

    Refactor validation for filter parameter in GET /api/users route

--- express_tut/src/routes/users.js
+++ express_tut/src/routes/users.js
@@ -1,1 +1,28 @@
 import { Router } from "express";
+import { query, validationResult } from "express-validator";
+
+const router = Router();
+
+router.get('/api/users',
+  query('filter')
+    .isString()
+    .notEmpty()
+    .withMessage('must not be empty')
+    .isLength({ min: 3, max: 10 })
+    .withMessage('filter must be a string with length between 3 and 10'),
+  (req, res) => {
+    const results = validationResult(req);
+    console.log(results);
+    const {
+      query: {filter, value},
+    } = req;
+
+  // when filter and value are defined
+    if (filter && value) 
+      return res.send(dummyData.filter((user) => user[filter].includes(value)));
+
+    return res.send(dummyData);
+
+  });
+
+export default router;
--- express_tut/src/server.js
+++ express_tut/src/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { query, validationResult, body, matchedData, checkSchema } from 'express-validator';
 import{ UserValidationSchema } from './utils/validationSchema.js';
+import usersRouter from './routes/users.js';
 
 const app = express();
 app.use(express.json());
+app.use(usersRouter);
 
 const loggingMiddleware = (req, res, next) => {
   console.log(`${req.method} ${req.path}`);
@@ -32,28 +34,6 @@ const dummyData = [
   res.status(201).send({msg: 'Hello World!'});
 });
 
-app.get(
-  '/api/users',
-  query('filter')
-    .isString()
-    .notEmpty()
-    .withMessage('must not be empty')
-    .isLength({ min: 3, max: 10 })
-    .withMessage('filter must be a string with length between 3 and 10'),
-  (req, res) => {
-    const results = validationResult(req);
-    console.log(results);
-    const {
-      query: {filter, value},
-    } = req;
-
-  // when filter and value are defined
-    if (filter && value) 
-      return res.send(dummyData.filter((user) => user[filter].includes(value)));
-
-    return res.send(dummyData);
-
-  });
 
 app.post(
   '/api/users', checkSchema(UserValidationSchema),
