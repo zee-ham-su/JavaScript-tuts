af070a8627c273a3a3c0328826cb4c68b2804336
Hamza Sufian <hsufiian@yahoo.com>
2024-04-24 12:52:52 +0000

    product router

---
+++ express_tut/src/routes/product.js
@@ -0,0 +1,17 @@
+import { router } from 'express';
+
+const productRouter = router();
+
+router.get("/api/products", (req, res) => {
+  res.send([
+    { id: 1, name: 'phone', price: 1000.00 },
+    { id: 2, name: 'laptop', price: 2000.99 },
+    { id: 3, name: 'tablet', price: 1500.50 },
+    { id: 4, name: 'desktop', price: 2500.75 },
+  ]);
+});
+
+
+
+
+export default productRouter;
--- express_tut/src/server.js
+++ express_tut/src/server.js
@@ -1,24 +1,17 @@
 import express from 'express';
 import usersRouter from './routes/users.js';
+import productRouter from './routes/product.js';
 
 const app = express();
 app.use(express.json());
 app.use(usersRouter);
+app.use(productRouter);
 
 const PORT = process.env.PORT || 3000;
 
   app.get('/', (req, res) => {
   res.status(201).send({msg: 'Hello World!'});
 
-});
-
-app.get("/api/products", (req, res) => {
-  res.send([
-    { id: 1, name: 'phone', price: 1000.00 },
-    { id: 2, name: 'laptop', price: 2000.99 },
-    { id: 3, name: 'tablet', price: 1500.50 },
-    { id: 4, name: 'desktop', price: 2500.75 },
-  ]);
 });
 
 
