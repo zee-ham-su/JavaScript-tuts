f6f1ce8e7a8327ce3f270c1d69ce37687a85e0db
Hamza Sufian <hsufiian@yahoo.com>
2024-04-25 20:03:34 +0000

    Update maxAge value for cookie setting in server.js

--- express_tut/src/server.js
+++ express_tut/src/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
   app.get('/', (req, res) => {
     res.cookie('hello', 'world', {
-      maxAge: 60000,
+      maxAge: 60000 * 60,
     });
   res.status(201).send({msg: 'Hello World!'});
 
