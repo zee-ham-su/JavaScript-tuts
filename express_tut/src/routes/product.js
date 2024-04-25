2bb24685e82b4006169d8d82acdf11a86412ad13
Hamza Sufian <hsufiian@yahoo.com>
2024-04-25 20:12:14 +0000

    Add logging statement in product.js

--- express_tut/src/routes/product.js
+++ express_tut/src/routes/product.js
@@ -3,13 +3,13 @@ import { Router } from 'express';
 const productRouter = Router();
 
 productRouter.get("/api/products", (req, res) => {
+  console.log(req.headers.cookie);
   res.send([
     { id: 1, name: 'phone', price: 1000.00 },
     { id: 2, name: 'laptop', price: 2000.99 },
     { id: 3, name: 'tablet', price: 1500.50 },
     { id: 4, name: 'desktop', price: 2500.75 },
   ]);
-  console.log('products');
 });
 
 
