76a6ffab848193e5a64f8d2c93243fb3be43b779
Hamza Sufian <hsufiian@yahoo.com>
2024-04-25 20:15:02 +0000

    Add cookie-parser dependency and import in server.js

--- express_tut/src/server.js
+++ express_tut/src/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import routes from './routes/index.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
 app.use(express.json());
 app.use(routes);
+
 
 const PORT = process.env.PORT || 3000;
 
