557f797d6668bea7ac5567fce81c3413007fa758
Hamza Sufian <hsufiian@yahoo.com>
2024-04-25 20:31:20 +0000

    Refactor product.js to check for authorization before sending product data

--- express_tut/src/routes/product.js
+++ express_tut/src/routes/product.js
@@ -5,15 +5,16 @@ const productRouter = Router();
 productRouter.get("/api/products", (req, res) => {
   console.log(req.headers.cookie);
   console.log(req.cookies);
-  res.send([
-    { id: 1, name: 'phone', price: 1000.00 },
-    { id: 2, name: 'laptop', price: 2000.99 },
-    { id: 3, name: 'tablet', price: 1500.50 },
-    { id: 4, name: 'desktop', price: 2500.75 },
-  ]);
+  if (req.cookies.hello && req.cookies.hello === 'world') {
+    res.send([
+      { id: 1, name: 'phone', price: 1000.00 },
+      { id: 2, name: 'laptop', price: 2000.99 },
+      { id: 3, name: 'tablet', price: 1500.50 },
+      { id: 4, name: 'desktop', price: 2500.75 },
+    ]);
+  } else {
+    res.send('You are not authorized to view this content');
+  }
 });
-
-
-
 
 export default productRouter;
