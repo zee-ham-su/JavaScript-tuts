fef4ec17a03883f56f7d72285d8ddfaee39f7932
Hamza Sufian <hsufiian@yahoo.com>
2024-05-01 17:41:41 +0000

    Refactor local strategy in passport authentication and deserializeUser function

--- express_tut/src/strategies/local-strategy.js
+++ express_tut/src/strategies/local-strategy.js
@@ -2,9 +2,19 @@ import passport from "passport";
 import {Strategy} from "passport-local";
 import { dummyData } from "../utils/data.js";
 
-passport.serializeUser(() => {
-    console.log('serializeUser');
+passport.serializeUser((user, done) => {
+    done(null, user.id);
 }); 
+
+passport.deserializeUser((id, done) => {
+    try {
+        const findUser = dummyData.find((user) => user.id === id);
+        if (!findUser) throw new Error('User not found')
+        done(null, findUser);
+    } catch (error) {
+        done(error, null);
+    }
+    });
 
 export default passport.use(
     new Strategy({ usernameField: "email" },(username, password, done) => {
