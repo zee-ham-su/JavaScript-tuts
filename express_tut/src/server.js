45d495f53cda0c04c15238c3e1bf090b3d9caf8c
Hamza Sufian <hsufiian@yahoo.com>
2024-05-02 13:40:31 +0000

    Add mongoose package and connect to MongoDB in server.js

--- express_tut/src/server.js
+++ express_tut/src/server.js
@@ -4,9 +4,13 @@ import cookieParser from 'cookie-parser';
 import session from 'express-session';
 import passport from 'passport';
 import "./strategies/local-strategy.js"
-
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/express-tut')
+.then(() => console.log(' successfully connected to MongoDB'))
+ .catch((err) => console.log(err));
 
 app.use(express.json());
 app.use(cookieParser("helloworld"));
@@ -36,6 +40,14 @@ app.get('/api/auth/status', (req, res) => {
   console.log(req.session);
   return req.user ? res.send(req.user) : res.sendStatus
   (401);
+});
+
+app.post('/api/auth/logout', (req, res) => {
+  if (!req.user) return res.sendStatus(401);
+  req.logout((err) => {
+    if (err) return res.sendStatus(400);
+    return res.sendStatus(200);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
